d611526e84bd6af766438fd6b22552dc3b04fda2
Rubens Panfili <rubens.panfili@gmail.com>
2019-07-23 21:55:28 +0200

    fix async and service entity_id on cover_sunset script

--- python_scripts/cover_sunset.py
+++ python_scripts/cover_sunset.py
@@ -3,10 +3,11 @@
 presence = hass.states.get('group.all_devices')
 logger.debug("devices presence status is \"{}\"".format(presence))
 if presence is 'home':
-    hass.services.async_call(domain="python_script",
-                             service="cover_mode",
-                             service_data={"mode": "sera"})
+    hass.services.call(domain="python_script",
+                       service="cover_mode",
+                       service_data={"mode": "sera"})
 else:
     # even on "unknown" state
-    hass.services.async_call(domain="cover",
-                             service="close_cover")
+    hass.services.call(domain="cover",
+                       service="close_cover",
+                       service_data={"entity_id": "all"})
