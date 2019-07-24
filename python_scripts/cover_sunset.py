eac9cec8cf5a5558cb1073e46c573985d09b0412
Rubens Panfili <rubens.panfili@gmail.com>
2019-07-24 21:37:28 +0200

    fix cover_sunset python script state retrieval

--- python_scripts/cover_sunset.py
+++ python_scripts/cover_sunset.py
@@ -1,13 +1,15 @@
 """Manages roller shutters sunset routine"""
 
 presence = hass.states.get('group.family')
-logger.debug("Family presence status is \"{}\"".format(presence))
-if presence is 'home':
-    hass.services.call(domain="python_script",
-                       service="cover_mode",
-                       service_data={"mode": "sera"})
-else:
-    # even on "unknown" state
-    hass.services.call(domain="cover",
-                       service="close_cover",
-                       service_data={"entity_id": "all"})
+if presence is not None:
+    current_state = presence.state
+    logger.info("Family presence status is \"{}\"".format(current_state))
+    if current_state is 'home':
+        hass.services.call(domain="python_script",
+                           service="cover_mode",
+                           service_data={"mode": "sera"})
+    else:
+        # even on "unknown" state
+        hass.services.call(domain="cover",
+                           service="close_cover",
+                           service_data={"entity_id": "all"})
