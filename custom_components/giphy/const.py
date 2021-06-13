224d2de0737d0f79ff18f19aa6d535f3d4f07194
Rubens Panfili <rubens.panfili@gmail.com>
2021-06-13 15:01:56 +0200

    Giphy integration improvements, track also search terms

--- custom_components/giphy/const.py
+++ custom_components/giphy/const.py
@@ -4,3 +4,9 @@ DOMAIN = "giphy"
 
 DATA_UPDATE = "giphy_update"
 DEFAULT_NAME = "Giphy"
+
+
+CONF_SEARCHES = "searches"
+CONF_SEARCH_TERM = "search"
+CONF_ADD_ANOTHER = "add_another"
+CONF_SKIP_STEP="skip_step"
